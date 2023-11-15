59ed3603329b091dc669464a34e0284b0b1be7a2
lorre0322 <lorre0322@foxmail.com>
2023-11-15 20:05:09 +0800

    Site updated: 2023-11-15 20:05:09

--- music/music.js
+++ music/music.js
@@ -1,6 +1,1 @@
-const play_list=[
-    "/music/For_River.mp3",
-    "/music/His_Theme.mp3",
-    "/music/Shall_We_Dance.mp3",
-    "/music/星河.mp3",
-]
+const play_list=["/music/For_River.mp3","/music/His_Theme.mp3","/music/Shall_We_Dance.mp3","/music/星河.mp3"];
